21155018b22019b9442cfc22e23009e50091d178
Sander <info@mesander.com>
2024-11-02 14:48:32 +0100

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,20 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+end
+---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+require("remap")
+
+vim.api.nvim_set_keymap("n", "<leader>l", "<cmd>Lazy<CR>", { noremap = true })
+require("lazy").setup("plugins", {
+    change_detection = {
+        notify = false,
+    },
+    ui = {
+        size = { width = 0.64, height = 0.7 },
+        border = "single",
+    },
+})
